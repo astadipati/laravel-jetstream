2c73b7ee29c07630df19e9e88358a4b0244b84b5
astadipati <astadipati@outlook.com>
2022-03-24 21:13:13 +0700

    generate token & login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !\Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+        $token = $user->createToken('token-name')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Sukses',
+            'user'=> $user,
+            'token'=>$token
+        ], 200);
+    }
 }
